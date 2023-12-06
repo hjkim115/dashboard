037c1898e7bec5a057afc2908495e4cacb43ad51
hjkim115 <hjkim6321@gmail.com>
2023-12-07 00:55:31 +0800

    Cors Update

--- next.config.js
+++ next.config.js
@@ -6,11 +6,7 @@ module.exports = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-            // value: process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
-          },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
